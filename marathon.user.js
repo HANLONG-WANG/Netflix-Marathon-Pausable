69492e92acd237df718d27e35497cf7604ab5192
Shane Hughes <shughes@mozilla.com>
2025-09-25 13:26:53 -0700

    5.8.4 - fix amazon video

--- marathon.user.js
+++ marathon.user.js
@@ -10,7 +10,7 @@
 // @name:ru            Netflix Marathon (пауза)
 // @name:hi            नेटफ्लिक्स मैराथन (रोकने योग्य)
 // @namespace          https://github.com/aminomancer
-// @version            5.8.3
+// @version            5.8.4
 // @description        A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:en     A configurable script that automatically skips recaps, intros, credits, and ads, and clicks "next episode" prompts on Netflix, Amazon Prime Video, Hulu, HBO Max, Starz, Disney+, and Hotstar. Customizable hotkey to pause/resume the auto-skipping functionality. Alt + N for settings.
 // @description:zh-CN  一个可配置的脚本，可自动跳过重述、介绍、演职员表和广告，并点击 Netflix、Amazon Prime Video、Hulu、HBO Max、Starz、Disney+ 和 Hotstar 上的“下一集”提示。 可自定义的热键暂停/恢复自动跳过功能。 Alt + N 进行设置。
@@ -325,7 +325,9 @@ const methods = {
       this.skips -= 1;
       return;
     }
-    const player = document.getElementById("dv-web-player");
+    const player = document.querySelector(
+      "[id^='dv-web-player'].dv-player-fullscreen"
+    );
     if (!player || !player.classList.contains("dv-player-fullscreen")) {
       return;
     }
